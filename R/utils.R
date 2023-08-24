82462ac5538190f23b03ac07a400e24946d7e445
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-08-24 17:12:38 +1000

    add document for noRd

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,9 @@
-
-# helper function to get local file.path for test package dataset.
-
+#' helper function to get local file.path for test package dataset.
+#'
+#' @param path default location as NULL
+#'
+#' @return
+#' @noRd
 skmoose_example <- function(path = NULL) {
   if (is.null(path)) {
     dir(system.file("extdata", package = "skmoose"))
