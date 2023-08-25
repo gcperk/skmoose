3e59bf3bce5bb16547c2f4c69fc117644c4a3d9a
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-08-25 11:30:51 +1000

    add documentation

--- R/utils.R
+++ R/utils.R
@@ -4,6 +4,11 @@
 #'
 #' @return TRUE
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' skmoose_example()
+#' }
 skmoose_example <- function(path = NULL) {
   if (is.null(path)) {
     dir(system.file("extdata", package = "skmoose"))
@@ -11,3 +16,4 @@ skmoose_example <- function(path = NULL) {
     system.file("extdata", path, package = "skmoose", mustWork = TRUE)
   }
 }
+
