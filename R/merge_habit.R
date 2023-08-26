1bea5bbaf3f4a5af75e8033834f5a1524e8b906d
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-08-26 10:19:43 +1000

    add habit merge function

---
+++ R/merge_habit.R
@@ -0,0 +1,90 @@
+#' Merge habitat areas
+#'
+#' @param tmp_aoi sf object with aoi of interest
+#' @param temp_out_dir file path where all prepared spatial files
+#'
+#' @return TRUE
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' merge_nonhabitat(tmp_aoi, temp_out_dir)
+#' }
+
+merge_habit <- function(tmp_aoi, temp_out_dir) {
+
+
+  hab_sf <- c("vri_decid.gpkg",
+                  "cutblocks_filtered.gpkg",
+                  "fires_filtered.gpkg",
+                  "streams38.gpkg",
+                  "streams9.gpkg",
+                  "wetland_filter.gpkg")
+
+
+  if(any(hab_sf %in% list.files(temp_out_dir))){
+    print("contains uninhabitable areas")
+
+    # create blank sf object to add to
+    hab <-  sf::st_sf(st_sfc(), crs = 3005)
+
+    # check deciduous
+    if(file.exists(file.path(temp_out_dir,"vri_decid.gpkg"))){
+      decid <- sf::st_read(file.path(temp_out_dir,"vri_decid.gpkg"))
+      hab<- rbind(decid, hab)
+      hab <- sf::st_union(hab)
+    }
+
+    # check recent cutblocks
+    if(file.exists(file.path(temp_out_dir,"cutblocks_filtered.gpkg"))){
+      harvest<- sf::st_read(file.path(temp_out_dir,"cutblocks_filtered.gpkg"))
+      hab <- sf::st_union(harvest,hab)
+      hab <- sf::st_union(hab)
+    }
+
+    # check fires
+    if(file.exists(file.path(temp_out_dir,"fires_filtered.gpkg"))){
+      fires <- sf::st_read(file.path(temp_out_dir,"fires_filtered.gpkg"))
+      hab <- sf::st_union(fires, hab)
+      hab <- sf::st_union(hab)
+    }
+
+    # check streams 3 - 8
+    if(file.exists(file.path(temp_out_dir,"streams38.gpkg"))){
+      st38<- sf::st_read(file.path(temp_out_dir,"streams38.gpkg"))
+      hab  <- sf::st_union(st38, hab)
+      hab <- sf::st_union(hab)
+    }
+
+    # check streams 9
+    if(file.exists(file.path(temp_out_dir,"streams9.gpkg"))){
+      st9 <- sf::st_read(file.path(temp_out_dir,"streams9.gpkg"))
+      hab  <- sf::st_union(st9, hab)
+      hab <- sf::st_union(hab)
+    }
+
+
+    # check wetlands
+    if(file.exists(file.path(temp_out_dir,"wetland_filter.gpkg"))){
+      wet<- sf::st_read(file.path(temp_out_dir,"wetland_filter.gpkg"))
+      hab  <- sf::st_union(wet, hab)
+      hab <- sf::st_union(hab)
+    }
+
+
+    # crop to the study area
+    hab_all = sf::st_intersection(hab, tmp_aoi)
+    st_write(hab_all, file.path(temp_out_dir, "habitable.gpkg"),
+             append = FALSE)
+
+    #return(unhab_all)
+
+  } else {
+
+    print("no habitable area found")
+
+  }
+  return(TRUE)
+}
+
+
--- R/merge_nonhabit.R
+++ R/merge_nonhabit.R
@@ -3,7 +3,7 @@
 #' @param tmp_aoi sf object with aoi of interest
 #' @param temp_out_dir file path where all prepared spatial files
 #'
-#' @return sf object
+#' @return TRUE
 #' @export
 #'
 #' @examples
@@ -18,54 +18,51 @@ merge_nonhabit <- function(tmp_aoi, temp_out_dir) {
                   "steep.gpkg","largelakes.gpkg")
 
 
-  if(uninhab_sf %in% list.files(temp_out_dir)){
+  if(any(uninhab_sf %in% list.files(temp_out_dir))){
     print("contains uninhabitable areas")
 
     unh <-  sf::st_sf(st_sfc(), crs = 3005)
 
+
     if(file.exists(file.path(temp_out_dir,"high_elevation.gpkg"))){
       high_elev <- sf::st_read(file.path(temp_out_dir,"high_elevation.gpkg"))
-      unh <- sf::st_union( high_elev_sf, unh)
+      unh <- rbind(high_elev, unh)
+      unh <- sf::st_union(unh)
 
     }
 
     if(file.exists(file.path(temp_out_dir,"rockice.gpkg"))){
-
-      rock_ice<- sf::st_read(file.path(temp_out_dir,"rockice.gpkg"))
-
-      unh <- sf::st_union( rockice, unh)
+      rockice<- sf::st_read(file.path(temp_out_dir,"rockice.gpkg"))
+      unh <- rbind(rockice, unh)
+      unh <- sf::st_union(unh)
     }
 
 
     if(file.exists(file.path(temp_out_dir,"steep.gpkg"))){
-
       steep<- sf::st_read(file.path(temp_out_dir,"steep.gpkg"))
-
+      unh <- rbind(steep, unh)
       unh <- sf::st_union(steep, unh)
     }
 
 
     if(file.exists(file.path(temp_out_dir,"largelakes.gpkg"))){
-
       largelakes<- sf::st_read(file.path(temp_out_dir,"largelakes.gpkg"))
-
+      unh <- rbind(largelakes, unh)
       unh <- sf::st_union(largelakes, unh)
-
     }
 
     # crop to the study area
-    unhab_all = sf::st_intersection(unh,  tmp_aoi)
-   # st_write(unhab_all, file.path(temp_out_dir, "uninhabitable.gpkg"),          append = FALSE)
+    unhab_all = sf::st_intersection(unh, tmp_aoi)
+    st_write(unhab_all, file.path(temp_out_dir, "uninhabitable.gpkg"),          append = FALSE)
 
-    return(unhab_all)
+    #return(unhab_all)
 
   } else {
 
     print("no uninhabitable area found")
 
   }
-
-
+  return(TRUE)
 }
 
 
