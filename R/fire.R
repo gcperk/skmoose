7ae88008253e0f61dd72cfeda27e21510b78887d
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-10-30 11:25:20 -0700

    update fire defaults

--- R/fire.R
+++ R/fire.R
@@ -1,17 +1,17 @@
 #' Select recent fire years for Moose habitat
 #' @param fires sf object with all fires
-#' @param fire_min_yr numeric minimum age since fire, default = 5.
-#' @param fire_max_yr numeric maximum age since fire, default = 40
+#' @param fire_min_yr numeric minimum age since fire, default = 10.
+#' @param fire_max_yr numeric maximum age since fire, default = 25
 #'
 #' @return sf object with filtered fires
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' fires_recent(fires, 5, 40 )
+#' fires_recent(fires, 10, 25 )
 #' }
 #'
-fires_recent <- function(fires, fire_min_yr = 5, fire_max_yr = 40){
+fires_recent <- function(fires, fire_min_yr = 10, fire_max_yr = 25){
 
   # filter for recent fires
   fires_filt <- fires %>%
@@ -23,4 +23,7 @@ fires_recent <- function(fires, fire_min_yr = 5, fire_max_yr = 40){
   return(fires_filt)
 
 }
+
+# fire severity
+#https://catalogue.data.gov.bc.ca/dataset/04c5ad28-d8eb-4c49-90c5-48b9b98fdfe9
 
