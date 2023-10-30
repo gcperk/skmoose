807ed6e35cfec9944629e670eac7f567b17befd5
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-10-30 11:25:11 -0700

    update cut dates default

--- R/cutblocks.R
+++ R/cutblocks.R
@@ -1,17 +1,17 @@
 #' Select recent cutblocks for Moose habitat
 #' @param cutblocks sf object with all cutblocks
 #' @param cutblock_min_yr numeric minimum age since date, default = 5.
-#' @param cutblock_max_yr numeric maximum age since harvest, default = 40
+#' @param cutblock_max_yr numeric maximum age since harvest, default = 25
 #'
 #' @return sf object with filtered cutblocks
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' cutblocks_recent(cutblocks, 5, 40 )
+#' cutblocks_recent(cutblocks, 5, 25 )
 #' }
 #'
-cutblocks_recent <- function(cutblocks, cutblock_min_yr = 5, cutblock_max_yr = 40){
+cutblocks_recent <- function(cutblocks, cutblock_min_yr = 5, cutblock_max_yr = 25){
 
   cutblocks_yrs <- cutblocks %>%
     dplyr::mutate(yrs_since_burn = (as.numeric(format(Sys.time(), "%Y")) - HARVEST_YEAR))%>%
