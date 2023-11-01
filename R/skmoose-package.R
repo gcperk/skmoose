13f33ea6cb0a724e9d8757e2d0e66562bf71262f
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2023-11-01 11:18:13 -0700

    updte imports

--- R/skmoose-package.R
+++ R/skmoose-package.R
@@ -2,6 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom fs path_package
+#' @importFrom magrittr %>%
+#' @importFrom rmapshaper ms_erase
+#' @importFrom stats quantile
 #' @importFrom stats quartile
 ## usethis namespace: end
 NULL
